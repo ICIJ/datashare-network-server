24efcd3b2998d6e8358683d30424e457a7684468
Bruno Thomas <bthomas@icij.org>
2022-01-07 11:51:31 +0000

    refactor: replace FastAPI with Starlette

--- dsnetserver/main.py
+++ dsnetserver/main.py
@@ -1,10 +1,15 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
 from dsnetserver import __version__
-from fastapi import FastAPI
-
-app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Datashare Network Server version %s" % __version__}
+async def homepage(_):
+    return JSONResponse({"message": "Datashare Network Server version %s" % __version__})
 
+routes = [
+    Route('/', homepage)
+]
+
+app = Starlette(debug=True, routes=routes)
